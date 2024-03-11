ccbe3b08e4363816cdc41374ad8c60f9e14d6072
Erdem <erdemcanaz@gmail.com>
2024-03-11 18:39:20 +0300

    Changing the algortihm

--- IR-main/IR_unit_11_03_2024-Erdem/IR_module_header.h
+++ IR-main/IR_unit_11_03_2024-Erdem/IR_module_header.h
@@ -9,7 +9,7 @@
 //delayMicroseconds() very accurately in the range 3 microseconds and up to 16383. Be careful if you are not in this range
 #define K_NUMBER_OF_BURSTS 60
 #define BURST_HALF_PERIOD_US 13
-#define LISTEN_DURATION_MS 20
+#define LISTEN_DURATION_MS 50
 #define TRANSMIT_SLEEP_TIME 20
 
 
--- IR-main/IR_unit_11_03_2024-Erdem/IR_unit_11_03_2024-Erdem.ino
+++ IR-main/IR_unit_11_03_2024-Erdem/IR_unit_11_03_2024-Erdem.ino
@@ -7,6 +7,7 @@ void setup() {
   set_number_of_package_bytes(4);
 }
 
+
 uint8_t mode = 0;  // 0: Receiver mode, 1: Transmitter mode
 uint8_t sub_mode = 0;
 
@@ -19,16 +20,16 @@ unsigned long timer_tic = 0;
 unsigned long timer_toc = 0;
 
 unsigned long DELAY_AFTER_PING_MS = 5;
+unsigned long DELAY_BEFORE_SENDING_ACK_MS = 10;
 unsigned long WAIT_PING_ACK_TIMEOUT_MS = 25;
-unsigned long DELAY_AFTER_ACK_IS_RECEIVED_MS = 250;
+unsigned long DELAY_AFTER_ACK_IS_RECEIVED_MS = 75;
 unsigned long DELAY_BETWEEN_DATA_PACKAGES_MS = 25;
+unsigned long LISTEN_TIMEOUT_MS = 1000;
 
 void loop() {
 
-  set_data_package(43, 58);  // first_byte, second_byte
-
-  if (mode == 1) { //TRANSMITTER MODE: =================================================================================================
-    if (sub_mode == 0) {                                                      //Send a pinging message
+  if (mode == 1) {        //TRANSMITTER MODE: =================================================================================================
+    if (sub_mode == 0) {  //Send a pinging message
       set_ping_package(ROBOT_ID, NUMBER_OF_DATA_PACKAGES, ROBOT_X, ROBOT_Y);
       transmit_buffer();
       delay(DELAY_AFTER_PING_MS);  //To avoid the robot is triggering itself. This ensures receiver is not triggered by this robot. Do not know whether usefull or not.
@@ -38,6 +39,7 @@ void loop() {
       if (millis() - timer_tic > WAIT_PING_ACK_TIMEOUT_MS) {  //no ACK is received for ping
         mode = 0;
         sub_mode = 0;
+        timer_tic = millis();
       }
       if (is_receiving_signal() == 1) {
         sub_mode = 2;
@@ -45,16 +47,35 @@ void loop() {
       }
 
     } else if (sub_mode == 2) {  // ACK is received, stream data
-      for(uint8_t i = 0 ; i < NUMBER_OF_DATA_PACKAGES; i++){
-        set_data_package(i, i<<1);
+      for (uint8_t i = 0; i < NUMBER_OF_DATA_PACKAGES; i++) {
+        set_data_package(i, i << 1);
         transmit_buffer();
         delay(DELAY_BETWEEN_DATA_PACKAGES_MS);
-      }    
+      }
       mode = 0;
       sub_mode = 0;
+      timer_tic = millis();
     }
-  } else if (mode == 0) {//RECEIVER MODE: =================================================================================================
-    delay(random(1000, 5000));
+  } else if (mode == 0) {  //RECEIVER MODE: =================================================================================================
+    if (millis()-timer_tic > LISTEN_TIMEOUT_MS){
+      mode = 1;
+      sub_mode = 0;
+      timer_tic = millis();
+    }
+    if (sub_mode == 0) {                       //Wait for ping signal
+      uint8_t listening_result = listen_IR();  //listens for 50ms (Check header!). 0:no package, 1:successful package, 2:corrupted package
+      //50 to 100ms is passed since started to listening
+      if (listening_result == 1) {
+        uint8_t second_byte = get_buffer(1); // x (4 bit), y(4 bit)
+        uint8_t x = second_byte >>4;
+        uint8_t y = second_byte %16;
+        Serial.println();
+        Serial.println(x);
+        Serial.println(y);
+        delay(DELAY_BEFORE_SENDING_ACK_MS)
+        
+      }
+    }
     mode = 1;
     sub_mode = 0;
   }
@@ -76,7 +97,6 @@ void set_ping_package(uint8_t ID, uint8_t number_of_packages, uint8_t x, uint8_t
   set_buffer(2, CRC_SIG);
   set_buffer(3, CRC_LST);
 }
-
 void set_data_package(uint8_t first_byte, uint8_t second_byte) {
   set_buffer(0, first_byte);
   set_buffer(1, second_byte);
@@ -86,91 +106,4 @@ void set_data_package(uint8_t first_byte, uint8_t second_byte) {
   uint8_t CRC_LST = CRC_16 % 256;
   set_buffer(2, CRC_SIG);
   set_buffer(3, CRC_LST);
-}
-
-uint8_t mode_of_operation = 0;  //0:listen, 1:ping
-unsigned long last_mode_change = 0;
-
-void ping_test() {
-  if (mode_of_operation == 0) return;
-  Serial.println("\n Pinging the IR module...");
-
-
-
-  //INFO ---------------
-  uint8_t x = 3;  // 4 bits
-  uint8_t y = 4;  // 4 bits
-
-  uint8_t intention = 0;  //4 bits
-  uint8_t ID = 2;         //4 bits
-
-  uint8_t first_byte = x * 16 + y;  //x and y position of the robot
-  uint8_t second_byte = 12;         //intention and ID of the robot
-
-  //TRANSMIT ---------------
-  set_buffer(0, first_byte);
-  set_buffer(1, second_byte);
-
-  uint16_t CRC_16 = generate_CRC_16_bit();
-  uint8_t CRC_SIG = CRC_16 >> 8;
-  uint8_t CRC_LST = CRC_16 % 256;
-  set_buffer(2, CRC_SIG);
-  set_buffer(3, CRC_LST);
-
-  unsigned int current_time = millis() / 1000;
-
-  transmit_buffer();
-
-  // long delay_after_ping = random(20, 100);
-  // delay(delay_after_ping);
-  mode_of_operation = 0;
-
-  // uint8_t listening_result = listen_IR(); //listens for 20ms. 0:no package, 1:successful package, 2:corrupted package
-
-  // if (listening_result != 0){
-  //   set_buffer(0, 12);
-  //   set_buffer(1, 38);
-
-  //   uint16_t CRC_16 = generate_CRC_16_bit();
-  //   uint8_t CRC_SIG = CRC_16 >> 8;
-  //   uint8_t CRC_LST = CRC_16 % 256;
-  //   set_buffer(2, CRC_SIG);
-  //   set_buffer(3, CRC_LST);
-  //   transmit_buffer();
-  // }
-}
-
-void listen_test() {
-  if (mode_of_operation == 1) return;
-
-  unsigned long listen_duration = random(500, 600);
-
-  // Serial.println("\nThe random number is: ");
-  // Serial.println(random(1000,2000));
-
-  Serial.println("\n Currently Listening...");
-  unsigned long start_time = millis();
-
-  while (millis() - start_time < listen_duration) {
-    uint8_t listening_result = listen_IR();  //listens for 20ms. 0:no package, 1:successful package, 2:corrupted package
-    if (listening_result == 1) {
-      Serial.println("Package is received");
-
-      uint8_t first_byte = get_buffer(0);
-      uint8_t second_byte = get_buffer(1);
-
-      uint8_t x = first_byte >> 4;
-      uint8_t y = first_byte % 16;
-
-      uint8_t intention = second_byte >> 4;
-      uint8_t ID = second_byte % 16;
-
-      Serial.println("x: " + String(x) + " y: " + String(y) + " intention: " + String(intention) + " ID: " + String(ID));
-      break;
-    }
-  }
-  mode_of_operation = 1;
-
-  //check for ping (intention check)
-  //if ping is received, send a ping back
 }
