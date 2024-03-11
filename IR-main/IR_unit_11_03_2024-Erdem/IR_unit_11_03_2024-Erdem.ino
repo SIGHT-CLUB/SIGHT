284d2e38dd035bfd39b0bc4d62ef55b9440cda47
Erdem <erdemcanaz@gmail.com>
2024-03-11 17:47:01 +0300

    Algorithm.

--- IR-main/IR_unit_11_03_2024-Erdem/IR_unit_11_03_2024-Erdem.ino
+++ IR-main/IR_unit_11_03_2024-Erdem/IR_unit_11_03_2024-Erdem.ino
@@ -10,24 +10,43 @@ void setup() {
 
 
 uint8_t mode = 0;  // 0: Receiver mode, 1: Transmitter mode
+uint8_t sub_mode = 0; 
+
+unsigned long timer_tic = 0;
+unsigned long timer_toc = 0;
+
+unsigned long WAIT_PING_ACK_TIMEOUT_MS = 50;
+
 void loop() {
-  set_ping_package(2,3,3,7); // ID, #Packages, x, y
+  
+  set_data_package(43,58); // first_byte, second_byte
+
+  if (mode == 1){//Transmitter mode  
+    if(sub_mode == 0){//Send a pinging message
+      set_ping_package(2,3,3,7); // ID, #Packages, x, y
+      transmit_buffer();
+      sub_mode = 1;
+      timer_tic = millis();
+    }
+    else if(sub_mode == 1){
+      if(millis()-timer_tic > WAIT_PING_ACK_TIMEOUT_MS){ //no ACK is received for ping
+        mode = 0;
+        sub_mode = 0;
+      }
+    }
+  }
+
 }
 
 void set_ping_package(uint8_t ID, uint8_t number_of_packages, uint8_t x, uint8_t y) {
   uint8_t PING_INTENTION_NO = 0;
   //Most sig to least significant -> ID (3 bit), Number of packages to be sent (2 bit), Intention(3 bit)
 
-
   uint8_t first_byte = (ID<<5) + (number_of_packages<<3) + (PING_INTENTION_NO); 
   uint8_t second_byte = (x<<4) + y;
 
   set_buffer(0, first_byte);
   set_buffer(1, second_byte);
-
-  Serial.println(first_byte);
-  Serial.println(second_byte);
-  Serial.println();
 
   uint16_t CRC_16 = generate_CRC_16_bit();
   uint8_t CRC_SIG = CRC_16 >> 8;
@@ -37,8 +56,15 @@ void set_ping_package(uint8_t ID, uint8_t number_of_packages, uint8_t x, uint8_t
 
 }
 
-void set_data_package(uint8_t byte_1, uint8_t byte_2) {
+void set_data_package(uint8_t first_byte, uint8_t second_byte) {
+  set_buffer(0, first_byte);
+  set_buffer(1, second_byte);
 
+  uint16_t CRC_16 = generate_CRC_16_bit();
+  uint8_t CRC_SIG = CRC_16 >> 8;
+  uint8_t CRC_LST = CRC_16 % 256;
+  set_buffer(2, CRC_SIG);
+  set_buffer(3, CRC_LST);
 }
 
 uint8_t mode_of_operation = 0;  //0:listen, 1:ping
@@ -126,77 +152,4 @@ void listen_test() {
 
   //check for ping (intention check)
   //if ping is received, send a ping back
-}
-
-
-void communication_test() {
-  //To be used in test 1 and test 2
-  static unsigned long succesful_package_counter = 0;
-  static unsigned long corrupted_package_counter = 0;
-
-  uint8_t listening_result = listen_IR();
-  if (listening_result == 1) {
-    succesful_package_counter = succesful_package_counter + 1;
-  } else if (listening_result == 2) {
-    corrupted_package_counter = corrupted_package_counter + 1;
-  } else if (listening_result == 0) {
-    //pass (no data is received)
-  }
-
-  if (Serial.available() > 0) {
-    int incoming_byte = Serial.read();
-    uint16_t number_of_package_bytes = get_number_of_package_bytes();
-
-    if (incoming_byte == 'S') {
-      Serial.println("Sending the data...");
-      for (uint16_t i = 0; i < 256; i++) {
-        for (uint16_t j = 0; j < number_of_package_bytes - 2; j++) {
-          set_buffer(j, i);
-        }
-        uint16_t CRC_16 = generate_CRC_16_bit();
-        uint8_t CRC_SIG = CRC_16 >> 8;
-        uint8_t CRC_LST = CRC_16 % 256;
-        set_buffer(number_of_package_bytes - 2, CRC_SIG);
-        set_buffer(number_of_package_bytes - 1, CRC_LST);
-        transmit_buffer();
-        Serial.println(String(i) + " / " + String(255));
-        delay(25);
-      }
-      Serial.println("Transmitting process is completed.");
-    } else if (incoming_byte == 'B') {
-      Serial.println("\nSending random data...");
-      for (uint16_t i = 0; i < number_of_package_bytes - 2; i++) {
-        uint8_t rand_number = random(256);
-        set_buffer(i, rand_number);
-        Serial.print(String(rand_number) + " ");
-      }
-      uint16_t CRC_16 = generate_CRC_16_bit();
-      uint8_t CRC_SIG = CRC_16 >> 8;
-      uint8_t CRC_LST = CRC_16 % 256;
-      Serial.println(String(CRC_SIG) + " " + String(CRC_LST));
-      set_buffer(number_of_package_bytes - 2, CRC_SIG);
-      set_buffer(number_of_package_bytes - 1, CRC_LST);
-
-      transmit_buffer();
-      delay(20);
-    } else if (incoming_byte == 'P') {
-      while (Serial.available() == 0) {}
-      int package_number = Serial.read();
-      if ('3' <= package_number && package_number <= '8') {
-        set_number_of_package_bytes(package_number - '0');
-        Serial.println("Number of package byte is set to: " + String(package_number - '0'));
-      } else {
-        Serial.println("Error: Number of package is not in limits");
-      }
-    } else if (incoming_byte == 'L') {
-      Serial.println("\nTotal packages received:" + String(succesful_package_counter + corrupted_package_counter));
-      Serial.println("Successful packages:" + String(succesful_package_counter));
-      Serial.println("Corrupted packages:" + String(corrupted_package_counter));
-      Serial.println("Success ratio is left as an exercise = " + String(succesful_package_counter) + " / " + String(succesful_package_counter + corrupted_package_counter));
-    } else if (incoming_byte == 'R') {
-      Serial.println("\nCounters are reseted");
-      succesful_package_counter = 0;
-      corrupted_package_counter = 0;
-    }
-  }
 }
