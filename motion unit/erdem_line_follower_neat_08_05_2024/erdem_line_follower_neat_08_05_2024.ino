1cf9f8f9efee8ee5716b17b067b07042e45261f6
Erdem <erdemcanaz@gmail.com>
2024-05-08 22:55:18 +0300

    trial-and error code

--- motion unit/erdem_line_follower_neat_08_05_2024/erdem_line_follower_neat_08_05_2024.ino
+++ motion unit/erdem_line_follower_neat_08_05_2024/erdem_line_follower_neat_08_05_2024.ino
@@ -41,10 +41,12 @@ void loop() {
   // update_black_detections();
   // test_print_is_black_array();
 
-  // move_forward_until_line_crossing(3000);
-  // open_loop_go_forward(180);
+  move_forward_until_line_crossing(5000);
+  delay(1000);
+  open_loop_go_forward_smooth(325);
+  delay(1000);
+  open_loop_turn_right_smooth(210);
   delay(2500);
-  open_loop_turn_right(180);
 }
 
 uint8_t is_black[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };  // if i'th sensor is black, set to 1. otherwise 0
@@ -85,7 +87,7 @@ void move_forward_until_line_crossing(unsigned long timeout_ms) {
       //TODO: maybe go forward a little so that RFID reader is read.
       break;
     } else if (number_of_blacks == 0) {  //line is lost, STOP if it is not found for a while using the last line position
-      if (millis() - line_found_when > 150 && line_position != -99) {
+      if (millis() - line_found_when > 500 && line_position != -99) {
         analogWrite(MOTOR1_PWM, 0);
         analogWrite(MOTOR2_PWM, 0);
         line_position = -99;
@@ -106,13 +108,21 @@ void move_forward_until_line_crossing(unsigned long timeout_ms) {
       if (is_black[i] == 1) line_position += (i - (3.5));
     }
     line_position = line_position / float(number_of_blacks);  // Ensure that this is a float division
-    uint8_t BASE_PWM = 215;                                   // This is an emprical value which is found to be fine. Neither halts nor accelerates too fast.
+    uint8_t BASE_PWM = 200;                                   // This is an emprical value which is found to be fine. Neither halts nor accelerates too fast.
 
-    uint8_t K_p = 7;
+    uint8_t K_p = 6;
     float del_PWM = K_p * line_position;  //line_pos is bound to [-3.5, 3.5]
 
     int motor1_PWM = BASE_PWM - int(del_PWM);
     int motor2_PWM = BASE_PWM + int(del_PWM);
+
+    // int motor1_PWM = BASE_PWM;
+    // int motor2_PWM = BASE_PWM;
+    // if (del_PWM < 0) {
+    //   motor1_PWM += -del_PWM;
+    // } else {
+    //   motor2_PWM += del_PWM;
+    // }
 
     //ensure that PWM values are bounded to [0,255];
     if (motor1_PWM > 255) motor1_PWM = 255;
@@ -166,6 +176,131 @@ void open_loop_turn_right(uint16_t duration) {
   delay(duration);
 
   //stop motors
+  analogWrite(MOTOR1_PWM, 0);
+  analogWrite(MOTOR2_PWM, 0);
+}
+
+void open_loop_turn_right_smooth(uint16_t duration) {
+
+  digitalWrite(MOTOR1_A, HIGH);
+  digitalWrite(MOTOR1_B, LOW);
+  digitalWrite(MOTOR2_A, HIGH);
+  digitalWrite(MOTOR2_B, LOW);
+
+
+  //set PWM values
+  analogWrite(MOTOR1_PWM, 254);
+  analogWrite(MOTOR2_PWM, 254);
+  delay(30);
+
+  //set PWM values
+  analogWrite(MOTOR1_PWM, 220);
+  analogWrite(MOTOR2_PWM, 220);
+
+  delay(duration);
+
+  //stop motors
+  analogWrite(MOTOR1_PWM, 0);
+  analogWrite(MOTOR2_PWM, 0);
+}
+
+void open_loop_go_forward_smooth(uint16_t duration) {
+  //Set directions so that bug tries to go forward
+  digitalWrite(MOTOR1_A, HIGH);
+  digitalWrite(MOTOR1_B, LOW);
+  digitalWrite(MOTOR2_A, LOW);
+  digitalWrite(MOTOR2_B, HIGH);
+
+  analogWrite(MOTOR1_PWM, 254);
+  analogWrite(MOTOR2_PWM, 254);
+  delay(30);
+
+  //set PWM values
+  analogWrite(MOTOR1_PWM, 240);
+  analogWrite(MOTOR2_PWM, 240);
+
+  delay(duration);
+
+  //stop motors
+  analogWrite(MOTOR1_PWM, 0);
+  analogWrite(MOTOR2_PWM, 0);
+}
+
+void closed_loop_turn_right(uint8_t number_of_pulses, uint16_t pulse_duration) {
+  for (uint8_t i = 0; i < number_of_pulses; i++) {
+    //Turn right in open-control manner.
+    digitalWrite(MOTOR1_A, HIGH);
+    digitalWrite(MOTOR1_B, LOW);
+    digitalWrite(MOTOR2_A, HIGH);
+    digitalWrite(MOTOR2_B, LOW);
+
+    //set PWM values
+    analogWrite(MOTOR1_PWM, 240);
+    analogWrite(MOTOR2_PWM, 240);
+
+    delay(pulse_duration);
+
+    //stop motors
+    analogWrite(MOTOR1_PWM, 0);
+    analogWrite(MOTOR2_PWM, 0);
+
+    delay(1000);  //wait until the motors are stopped
+
+    //check if black line is found, if found break. Otherwise keep continue until number of burst are complated.
+    update_black_detections();
+    uint8_t number_of_blacks = 0;
+    for (uint8_t i = 0; i < 8; i++) {
+      if (is_black[i] == 1) number_of_blacks += 1;
+    }
+    if (number_of_blacks > 0) break;
+  }
+}
+
+void closed_loop_turn_right_v2(unsigned long timeout_ms) {
+
+  //Turn right configuration
+  digitalWrite(MOTOR1_A, HIGH);
+  digitalWrite(MOTOR1_B, LOW);
+  digitalWrite(MOTOR2_A, HIGH);
+  digitalWrite(MOTOR2_B, LOW);
+
+
+  //escape from the current line
+  analogWrite(MOTOR1_PWM, 190);
+  analogWrite(MOTOR2_PWM, 190);
+
+  uint8_t number_of_blacks = 0;
+  unsigned long start_time = millis();
+  while (millis() - start_time < timeout_ms) {
+    update_black_detections();
+    for (uint8_t i = 0; i < 8; i++) {
+      if (is_black[i] == 1) number_of_blacks += 1;
+    }
+    if (number_of_blacks == 0) break;
+  }
+  delay(15);  //to avoid glitching in 0&1's at the edge
+
+  //find the next line
+  while (millis() - start_time < timeout_ms) {
+    update_black_detections();
+    for (uint8_t i = 0; i < 8; i++) {
+      if (is_black[i] == 1) number_of_blacks += 1;
+    }
+    if (number_of_blacks > 0) break;
+  }
+
+  delay(15);  //to ensure the line is centered
+
+  //Apply HARD BREAK!, Turn left configuration
+  digitalWrite(MOTOR1_A, LOW);
+  digitalWrite(MOTOR1_B, HIGH);
+  digitalWrite(MOTOR2_A, LOW);
+  digitalWrite(MOTOR2_B, HIGH);
+  analogWrite(MOTOR1_PWM, 240);
+  analogWrite(MOTOR2_PWM, 240);
+  delay(30);
+
+  //Stop the motors
   analogWrite(MOTOR1_PWM, 0);
   analogWrite(MOTOR2_PWM, 0);
 }
